d5182e7d8f5dbedc622cbba1635024b66d747486
lin jeff <jeff.lin@jyintgroup.com>
2022-01-09 19:34:45 +0800

    feat: update CustomerPart SliderOneButton

--- components/element/SliderOneButton.vue
+++ components/element/SliderOneButton.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="sliderbuttononeborder">
-    <nuxt-link to="/" class="sliderbuttonone">
-      Meet Our Clients
-    </nuxt-link>
+  <div class="sliderbuttononeborder" @click="reload">
+    <nuxt-link to="/" class="sliderbuttonone"> Meet Our Clients </nuxt-link>
   </div>
 </template>
 
@@ -16,30 +14,37 @@ export default {
     honetitle: {
       type: Object,
       default: () => {},
-    } 
+    },
+  },
+  methods: {
+    reload() {
+      setTimeout(() => {
+        window.location.reload()
+      }, 500)
+    },
   },
 }
 </script>
 
 <style>
-  .sliderbuttononeborder{
-    margin: 10px 0 55px 0;
-  }
-  .sliderbuttonone{
-    background:linear-gradient(to right, #145484 50%, #1868a4 50%);
-    background-size: 200% 100%;
-    background-position: right bottom;
-    font-weight: bolder;
-    padding: 15px 50px;
-    border-radius: 30px;
-    transition: all .5s ease-out;
-    color: white;
-  }
-  .sliderbuttonone:hover{
-    background-position: left bottom;
-    cursor: pointer;
-  }
-  .sliderbuttonone:not(:hover){
-    background-position: right bottom;
-  }
+.sliderbuttononeborder {
+  margin: 10px 0 55px 0;
+}
+.sliderbuttonone {
+  background: linear-gradient(to right, #145484 50%, #1868a4 50%);
+  background-size: 200% 100%;
+  background-position: right bottom;
+  font-weight: bolder;
+  padding: 15px 50px;
+  border-radius: 30px;
+  transition: all 0.5s ease-out;
+  color: white;
+}
+.sliderbuttonone:hover {
+  background-position: left bottom;
+  cursor: pointer;
+}
+.sliderbuttonone:not(:hover) {
+  background-position: right bottom;
+}
 </style>
