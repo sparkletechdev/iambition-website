db1fd0398e77eb909c1c35ba72cb91845a5dc30e
Teh Hsao Chai <hsaochai@gmail.com>
2022-03-09 16:58:02 +0800

    Updated footer styling, formatting. Fixed warnings.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,52 +1,46 @@
 <template>
-  <div class="footercontainer">
-    <div class="footerborder">
-      <div class="footertopborder">
-        <div class="footertoppartnersborder">
-          <p class="footerpartners">
-            Our Partners
-          </p>
-          <span class="footerpartnerslinehidden"></span>
-          <div class="footerpartnersicon">
-            <fa :icon="['fas', 'apple-alt']" class="colorblue"/>
-            <fa :icon="['fas', 'chess-knight']" class="colorblue" />
-            <fa :icon="['fas', 'dice-d20']" class="colorblue" />
-            <fa :icon="['fas', 'dice']" class="colorblue" />
-            <fa :icon="['fas', 'wind']" class="colorblue" />
+  <div class="footer-container">
+    <div class="footer-border">
+      <div class="footer-top-border">
+        <div class="footer-top-partners-border">
+          <p class="footer-partners">Our Partners</p>
+          <span class="footer-partners-line-hidden"></span>
+          <div class="footer-partners-icon">
+            <fa :icon="['fas', 'apple-alt']" class="color-blue" />
+            <fa :icon="['fas', 'chess-knight']" class="color-blue" />
+            <fa :icon="['fas', 'dice-d20']" class="color-blue" />
+            <fa :icon="['fas', 'dice']" class="color-blue" />
+            <fa :icon="['fas', 'wind']" class="color-blue" />
           </div>
         </div>
-        <div class="footertoplinksborder">
-          <a href="https://www.instagram.com" class="footerlinks">
-            <fa :icon="['fab', 'instagram-square']" class="colorbluesmall" />
+        <div class="footer-top-links-border">
+          <a href="https://www.instagram.com" class="footer-links">
+            <fa :icon="['fab', 'instagram-square']" class="color-blue-small" />
           </a>
-          <a href="https://zh-tw.facebook.com" class="footerlinks">
-            <fa :icon="['fab', 'facebook-square']" class="colorbluesmall" />
+          <a href="https://zh-tw.facebook.com" class="footer-links">
+            <fa :icon="['fab', 'facebook-square']" class="color-blue-small" />
           </a>
-          <a href="https://twitter.com" class="footerlinks">
-            <fa :icon="['fab', 'twitter-square']" class="colorbluesmall" />
+          <a href="https://twitter.com" class="footer-links">
+            <fa :icon="['fab', 'twitter-square']" class="color-blue-small" />
           </a>
-          <a href="https://tw.linkedin.com" class="footerlinks">
-            <fa :icon="['fab', 'linkedin']" class="colorbluesmall" />
+          <a href="https://tw.linkedin.com" class="footer-links">
+            <fa :icon="['fab', 'linkedin']" class="color-blue-small" />
           </a>
-          <a href="https://www.youtube.com" class="footerlinks">
-            <fa :icon="['fab', 'youtube-square']" class="colorbluesmall" />
+          <a href="https://www.youtube.com" class="footer-links">
+            <fa :icon="['fab', 'youtube-square']" class="color-blue-small" />
           </a>
         </div>
       </div>
       <!-- <span class="footerspan"></span> -->
-      <div class="fotterbottomborder">
-        <div class="footerbottomprivacyborder">
-          <p class="companyprivacy">
-            © 2022 Sparkle Technology Pte. Ltd.
-          </p>
-          <!-- <nuxt-link to="/privacy" class="privacylink">
+      <div class="footer-bottom-border">
+        <div class="footer-bottom-privacy-border">
+          <p class="company-privacy">© 2022 iAmbition Technology Pte. Ltd.</p>
+          <!-- <nuxt-link to="/privacy" class="privacy-link">
             Privacy Policy
           </nuxt-link> -->
         </div>
-        <div class="footerbottomlocation">
-          <p class="location">
-            Singapore | Taiwan
-          </p>
+        <div class="footer-bottom-location">
+          <p class="location">Singapore | Taiwan</p>
         </div>
       </div>
     </div>
@@ -54,137 +48,140 @@
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style>
-  .footercontainer{
-    /* border: 1px solid black; */
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.location {
+  color: white;
+}
+.footer-container {
+  /* border: 1px solid black; */
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #333;
+}
+.footer-container .footer-border {
+  /* border: 1px solid black; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1140px;
+  width: 1140px;
+}
+.footer-border .footer-top-border {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 0;
+  border-bottom: 0.4px solid #c9c9c9;
+}
+.footer-top-border .footer-top-partners-border {
+  display: flex;
+  align-items: center;
+}
+.footer-partners {
+  font-size: 16px;
+  color: white;
+  font-weight: 400;
+  margin-right: 25px;
+  opacity: 0.85;
+}
+.color-blue {
+  color: #369;
+  font-size: 2.6rem;
+  margin-right: 32px;
+}
+.color-blue-small {
+  color: #369;
+  font-size: 1.8rem;
+  margin-left: 18px;
+}
+.footer-container .footer-bottom-border {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+.footer-bottom-border .footer-bottom-privacy-border {
+  width: 40%;
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 0;
+}
+.company-privacy {
+  color: white;
+  opacity: 0.85;
+}
+.privacy-link {
+  color: #369;
+}
+@media screen and (max-width: 1300px) {
+  .footer-container .footer-border {
+    width: 900px;
   }
-  .footercontainer .footerborder{
-    /* border: 1px solid black; */
+}
+@media screen and (max-width: 992px) {
+  .footer-container .footer-border {
+    width: 700px;
+  }
+  .footer-border .footer-top-border {
     display: flex;
     flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
+  .footer-top-border .footer-top-partners-border {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 35px;
+  }
+}
+@media screen and (max-width: 786px) {
+  .footer-container .footer-border {
+    width: 100%;
+  }
+  .footer-border .footer-top-border {
+    border-bottom: none;
+  }
+  .footer-container .footer-bottom-border {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
+  .footer-bottom-border .footer-bottom-privacy-border {
+    width: 100%;
+    display: flex;
+    align-items: center;
     justify-content: center;
+  }
+}
+@media screen and (max-width: 576px) {
+  .footer-top-border .footer-top-partners-border {
+    display: flex;
+    flex-direction: column;
     align-items: center;
-    max-width: 1140px;
-    width: 1140px;
+    justify-content: center;
   }
-  .footerborder .footertopborder{
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 15px 0;
-    border-bottom: .4px solid #c9c9c9 ;
+  .footer-partners {
+    margin-bottom: 20px;
   }
-  .footertopborder .footertoppartnersborder{
-    display: flex;
-    align-items: center;
+  .color-blue {
+    margin: 0 10px;
   }
-  .footerpartners{
-    font-size: 16px;
-    color: #212529;
-    font-weight: 400;
-    margin-right: 25px;
-    opacity: .85;
+}
+@media screen and (max-width: 414px) {
+  .updatemail {
+    width: 260px;
   }
-  .colorblue{
-    color: #1868A4;
-    font-size: 2.6rem;
-    margin-right: 32px;
+  .updatebutton {
+    width: 90px;
+    font-size: 1rem;
+    height: 50px;
   }
-  .colorbluesmall{
-    color: #1868A4;
-    font-size: 1.8rem;
-    margin-left: 18px;
-  }
-  .footercontainer .fotterbottomborder{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-  }
-  .fotterbottomborder .footerbottomprivacyborder{
-    width: 40%;
-    display: flex;
-    justify-content: space-between;
-    padding: 15px 0;
-  }
-  .companyprivacy{
-    color: #212529;
-    opacity: .85;
-  }
-  .privacylink{
-    color: #1868A4;
-  }
-  @media screen and (max-width: 1300px) {
-    .footercontainer .footerborder {
-      width: 900px;
-    }
-  }
-  @media screen and (max-width: 992px) {
-    .footercontainer .footerborder {
-      width: 700px;
-    }
-    .footerborder .footertopborder{
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      width: 100%;
-    }
-    .footertopborder .footertoppartnersborder{
-      display: flex;
-      justify-content: space-between;
-      margin-bottom: 35px;
-    }
-  }
-  @media screen and (max-width: 786px) {
-    .footercontainer .footerborder {
-      width: 100%;
-    }
-    .footerborder .footertopborder{
-      border-bottom: none;
-    }
-    .footercontainer .fotterbottomborder{
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      width: 100%;
-    }
-    .fotterbottomborder .footerbottomprivacyborder {
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-  }
-  @media screen and (max-width: 576px) {
-    .footertopborder .footertoppartnersborder{
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
-    .footerpartners{
-      margin-bottom: 20px;
-    }
-    .colorblue{
-      margin: 0 10px;
-    }
-  }
-  @media screen and (max-width: 414px) {
-    .updatemail{
-      width: 260px;
-    }
-    .updatebutton{
-      width: 90px;
-      font-size: 1.0rem;
-      height: 50px;
-    }
-  }
+}
 </style>
