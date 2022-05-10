b7512a916ca4f92472552d447b8e119a2d03c35f
Teh Hsao Chai <hsaochai@gmail.com>
2022-05-11 01:25:39 +0800

    Updated customer.vue. Another attempted fixed for hooper css not being overridden.

--- pages/CUSTOMERS.vue
+++ pages/CUSTOMERS.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="customers-part-border">
-    <CustomerPart />
+    <!-- <CustomerPart /> -->
+    <LandingComponent />
     <CustomerIntroducePart />
     <SliderOne />
-    <UpdatePart />
   </main>
 </template>
 
@@ -27,5 +27,14 @@ export default {
 .customers-part-border {
   width: 100%;
   position: relative;
+}
+
+::v-deep .landing-container {
+  background-image: linear-gradient(
+      rgba(51, 102, 153, 0.75),
+      rgba(39, 50, 68, 1)
+    ),
+    url('~@/assets/images/client.jpg');
+    background-position: 50%;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,47 +9,6 @@
   </main>
 </template>
 
-<script>
-import 'hooper/dist/hooper.css'
-export default {}
-</script>
-<style>
-.hooper-indicator {
-  margin: 0 6px;
-  width: 12px;
-  height: 12px;
-  border-radius: 12px;
-  border: none;
-  background-color: #fff;
-  cursor: pointer;
-}
-.hooper-indicator:hover,
-.hooper-indicator.is-active {
-  background-color: #0cf;
-}
-.hooper-next,
-.hooper-prev {
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 2rem;
-  border: none;
-  padding: 0.5rem;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  cursor: pointer;
-}
-.hooper-next {
-  right: 8px;
-}
-.hooper-prev {
-  left: 8px;
-}
-.hooper-next:hover,
-.hooper-prev:hover {
-  background-color: rgba(0, 204, 255, 0.4);
-}
-</style>
-
 <style scoped>
 ::v-deep .landing-container {
   background-image: linear-gradient(
