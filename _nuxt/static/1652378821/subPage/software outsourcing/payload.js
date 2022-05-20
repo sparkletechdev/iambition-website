31c2a71f615e4fcbdb9dc026aa8e1e6f030a9e18
sparkletechdev <sparkletechdev@users.noreply.github.com>
2022-05-20 00:55:06 +0000

    deploy: 82f13d615feaee76ceee22d2aec0572219027392

--- _nuxt/static/1652378821/subPage/software outsourcing/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/subPage/software%20outsourcing", {data:[{}],fetch:{},mutations:[]});
--- _nuxt/static/1652378821/zh/subPage/software outsourcing/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/zh/subPage/software%20outsourcing", {data:[{}],fetch:{},mutations:[]});
